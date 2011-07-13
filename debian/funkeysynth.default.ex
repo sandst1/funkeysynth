aae094a089fa6936c659c3045ac6473635fc72ee
Topi Santakivi <topi.santakivi@gmail.com>
2011-07-13 17:50:23 +0300

    Restructuring the project

---
+++ debian/funkeysynth.default.ex
@@ -0,0 +1,10 @@
+# Defaults for funkeysynth initscript
+# sourced by /etc/init.d/funkeysynth
+# installed at /etc/default/funkeysynth by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(funkeysynth):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="funkeysynth" command="/usr/bin/funkeysynth"
