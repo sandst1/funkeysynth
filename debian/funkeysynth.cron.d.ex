9a3e3da00e30b2445376444cdce4b48d57262764
Topi Santakivi <topi.santakivi@gmail.com>
2011-07-16 12:21:34 +0300

    Clean unneccessary files from the debian folder

--- debian/funkeysynth.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the funkeysynth package
-#
-0 4	* * *	root	[ -x /usr/bin/funkeysynth_maintenance ] && /usr/bin/funkeysynth_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(funkeysynth):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="funkeysynth" command="/usr/bin/funkeysynth"
